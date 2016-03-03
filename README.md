51a8a3804493c7b906f9931f82bbd730ec6a8369
Jeremy Nevill <jeremy@nevill.net>
2016-03-03 23:15:16 +0000

    Additional vs plus readme mods.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # parkbench
+
+Concise Vagrantfile for standing up a Windows 2012 r2 .net Development Virtual Machine using Virtualbox.
+
--- scripts/003_InstallDevTools.bat
+++ scripts/003_InstallDevTools.bat
@@ -2,4 +2,5 @@ chocolatey feature enable -n=allowGlobalConfirmation
 choco install nodejs.install
 choco install git.install
 choco install visualstudiocode
+choco install visualstudio2015community
 chocolatey feature disable -n=allowGlobalConfirmation
